2b88aeef19158bc0231b58e65eb15acc29d9c80c
linyf <linyf@59store.com>
2017-08-13 19:29:48 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const Koa = require('koa')
+const app = new Koa()
+const views = require('koa-views')
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
+const logUtil = require('./utils/log_util.js')
+
+const index = require('./api/routes/index')
+const users = require('./api/routes/users')
+
+// error handler
+onerror(app)
+
+// middlewares
+app.use(bodyparser({
+  enableTypes:['json', 'form', 'text']
+}))
+app.use(json())
+app.use(logger())
+app.use(require('koa-static')(__dirname + '/public'))
+
+app.use(views(__dirname + '/views', {
+  extension: 'pug'
+}))
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date()
+  var ms
+  try{
+      await next()
+      ms = new Date() - start
+      logUtil.logResponse(ctx,ms)
+  } catch(error){
+      ms = new Date()-start
+      logUtil.logError(ctx,error,ms)
+  }
+})
+
+// routes
+app.use(index.routes(), index.allowedMethods())
+app.use(users.routes(), users.allowedMethods())
+
+module.exports = app
---
+++ config/env/development.js
@@ -0,0 +1,14 @@
+module.exports={
+    mysqlServer:{
+        host:'127.0.0.1',
+        user:'root',
+        port:3306,
+        password:'123456',
+        database:'water2017'
+    },
+    redisServer: {
+        host: '10.232.0.13',
+        port: 6379,
+        database: 7
+    }
+}
