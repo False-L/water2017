e76e39ce1d31d17890126df874681c1a81809531
false-l <waterservers@163.com>
2017-08-29 23:25:49 +0800

    update

--- water-admin/api/models/Forum.js
+++ water-admin/api/models/Forum.js
@@ -19,4 +19,44 @@ sequelize.define('forum',{
 })
 var ForumModel = sequelize.models.forum
 
+/**
+ * 初始化版块列表
+ */
+ForumModel.initialize = async function () {
+    let rawForums = await ForumModel.findAll()
+    rawForums = JSON.stringify(rawForums)
+    rawForums = JSON.parse(rawForums)
+    if(rawForums){
+      var handledForum = {};
+      var handledForumId = {};
+      var forumSelectList = [
+          {
+              key: '未选择',
+              value: ''
+          }
+      ]
+      for (var i in rawForums) {
+        
+        var forum = rawForums[i]
+        
+        if (forum) {
+            if(forum.header){
+                forum.header = forum.header.replace('@time',forum.cooldown).replace('@name',forum.name);
+            }
+            forum.version = _.random(100, 999);
+            handledForum[forum.name] = forum;
+            handledForumId[forum.id] = forum.name;
+            forumSelectList.push({
+              key: forum.name,
+              value: forum.id
+            })
+        }
+      }
+
+
+    }
+}
+
+
+
 module.exports = ForumModel
--- water-front/api/routes/index.js
+++ water-front/api/routes/index.js
@@ -7,7 +7,7 @@ router.get('/', HomepageController.index)
 router.get('/:format?', HomepageController.index)
 router.get('/homepage/menu/:format?',HomepageController.menu)
 // router.get('/homepage/ref/:format?',HomepageController.ref)
-// router.get('/homepage/isManager',HomepageController.isManager)
+router.get('/homepage/isManager',HomepageController.isManager)
 // router.get('/search/:format?',HomepageController.search)
 // router.get('/homepage/switchType',HomepageController.switchType)
 
